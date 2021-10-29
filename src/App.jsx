ae5220f1b9a95580f5e20aae27f712008d5ecf25
dburanov <dburanov.sup@yandex.ru>
2021-10-29 14:44:43 +0300

    add 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ import  PrivateRoute from './Services/PrivateRoute'
 import Login from "./Components/Login"
 import Register from "./Components/Register"
 import Home from "./Components/Home"
-import { history } from './Services/history'
+import { history } from "./Services/history"
+import Page404 from "./Components/404"
 
 
 
@@ -20,7 +21,8 @@ const App=() => {
                           <PrivateRoute exact path="/" component={Home} />
                           <Route path="/login" component={Login} />
                           <Route path="/register" component={Register} />
-                          <Redirect from="*" to="/" />
+                          <Route path="/404" component={Page404} />
+                          <Redirect from="*" to="/404" />
                       </Switch>
                   </Router>
               </Container>
