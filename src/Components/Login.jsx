306a5fed8e5466dd687bcef635a46ffb927c3846
dburanov <dburanov.sup@yandex.ru>
2021-10-29 13:12:48 +0300

    add fake backend

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,11 +1,9 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import {Form, Button, Container, Row, Col} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Form, Button, Container, Row, Col} from 'react-bootstrap';
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-
 import { userActions } from '../Auctions/user.auctions';
-
 
 function Login() {
   const [inputs, setInputs] = useState({
@@ -44,17 +42,18 @@ function Login() {
             <Row className="mt-5">
                 <Col lg={5} md={6} sm={12} className="p-5 m-auto shadow-sm rounded-lg">
                     <Form onSubmit={handleSubmit}>
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email"  value={username} onChange={handleChange} />
+                        <Form.Group className="mb-3">
+                            <Form.Label>Username</Form.Label>
+                            <Form.Control type="text" placeholder="Enter Username" name = "username" value={username} onChange={handleChange} />
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="formBasicPassword" >
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password"  value={username} onChange={handleChange}/>
+                            <Form.Control type="password" placeholder="Password" name = "password"  value={password} onChange={handleChange}/>
                         </Form.Group>
 
                         <Button variant="primary" type="submit">
+                        {loggingIn && <span className="spinner-border spinner-border-sm mr-1"></span>}
                             Login
                         </Button>
                         <Link to="/register" className="btn btn-link">Register</Link>
--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import  App  from './App';
 import { Provider } from 'react-redux';
-import { createStore} from 'redux';
+import { createStore, applyMiddleware} from 'redux';
 import rootReducer from './Reducers';
+import thunkMiddleware from 'redux-thunk';
+import { configureFakeBackend } from './Data/fake-backend'
+
+const middlewareEnhancer = applyMiddleware(thunkMiddleware)
+
+const store = createStore(rootReducer, undefined, middlewareEnhancer)
 
 
-const store = createStore(rootReducer)
+// setup fake backend
+configureFakeBackend();
 
 ReactDOM.render(
   <Provider store={store}>
